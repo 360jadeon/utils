941fea8bbee2370d5c00c50030140bb9cb4dc9d5
jadeon <jadeon@foxmail.com>
2019-11-25 15:01:40 +0800

    Create Apple.java

---
+++ java/src/main/java/com/jadeon/java8/entity/Apple.java
@@ -0,0 +1,20 @@
+package com.jadeon.java8.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Apple {
+
+    private String country;
+
+    private Long weight;
+
+    private String color;
+
+}
