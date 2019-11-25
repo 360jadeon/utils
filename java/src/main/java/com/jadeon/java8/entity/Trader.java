dfbd6be585292d7ff05d0d92093031a4e0d2849f
jadeon <jadeon@foxmail.com>
2019-11-25 16:38:52 +0800

    Create Trader.java

---
+++ java/src/main/java/com/jadeon/java8/entity/Trader.java
@@ -0,0 +1,16 @@
+package com.jadeon.java8.entity;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Trader {
+
+    private String name;
+
+    private String city;
+
+}
